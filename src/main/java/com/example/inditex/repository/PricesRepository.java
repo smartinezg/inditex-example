e92c03f296dc180e31dbb67efa190cc762bd77c1
Sergio Martínez <sergio.martinez.g@gmail.com>
2021-10-03 16:33:36 -0300

    fix Persistene

--- src/main/java/com/example/inditex/repository/PricesRepository.java
+++ src/main/java/com/example/inditex/repository/PricesRepository.java
@@ -5,7 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.inditex.persistence.Prices;
 
 public interface PricesRepository extends CrudRepository<Prices, Integer> {
-	
-	Iterable<Prices> findAll();
-
 }
